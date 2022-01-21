715381d440c6d2ff4d0286ae34fb418054692828
Kuba Maruszczyk <kmaruszczyk1604@gmail.com>
2022-01-21 23:33:15 +0000

    testing with gaussian clusters (2D) k-15

--- KMeans/Program.cs
+++ KMeans/Program.cs
@@ -8,16 +8,40 @@ namespace KMeans
 {
     class Program
     {
+
+        static List<IDataPoint> ReadTestData()
+        {
+            string path = "c:/users/kuba/desktop/data_test.txt";
+
+            List<IDataPoint> pts = new List<IDataPoint>();
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(path))
+            {
+                int index = 0;
+                while(!reader.EndOfStream)
+                {
+                    index++;
+                    string rd = reader.ReadLine();
+                    string[] el = rd.Split(',');
+                    el[0] = el[0].Trim();
+                    el[1] = el[1].Trim();
+                    double x = double.Parse(el[0]);
+                    double y = double.Parse(el[1]);
+                    pts.Add(new IDataPoint(new double[] { x, y }));
+                }
+            }
+            return pts;
+        }
         static void Main(string[] args)
         {
-
-            List<IDataPoint> points = new List<IDataPoint>();
-            Random rnd = new Random();
-            for (int i = 0; i < 50; ++i)
-            {
-                //points.Add(new IDataPoint(new double[] { rnd.NextDouble() * rnd.Next(0, 20), rnd.NextDouble() * rnd.Next(0, 20) }));
-                points.Add(new IDataPoint(new double[] { i,  i }));
-            }
+            ReadTestData();
+            List<IDataPoint> points = ReadTestData();
+                // new List<IDataPoint>();
+            //Random rnd = new Random();
+            //for (int i = 0; i < 50; ++i)
+            //{
+            //    //points.Add(new IDataPoint(new double[] { rnd.NextDouble() * rnd.Next(0, 20), rnd.NextDouble() * rnd.Next(0, 20) }));
+            //    points.Add(new IDataPoint(new double[] { i,  i }));
+            //}
 
             //foreach (var point in points)
             //{
@@ -25,7 +49,7 @@ namespace KMeans
             //}
             //Console.WriteLine();
 
-            KMeansClustering cl = new KMeansClustering(points.ToArray(), 5);
+            KMeansClustering cl = new KMeansClustering(points.ToArray(), 15);
             cl.PrintCentroids();
             Cluster[] clusters =  cl.Calculate();
             Console.WriteLine(clusters.Length);
